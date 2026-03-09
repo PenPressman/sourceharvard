214b45abe1ef37074410d267cd4ab23f37a62604
Penelope Pressman <110858570+PenPressman@users.noreply.github.com>
2026-03-09 17:08:05 -0400

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+     base: '/sourceharvard/',  // ← add this line
   plugins: [react()],
   test: {
     environment: "jsdom",

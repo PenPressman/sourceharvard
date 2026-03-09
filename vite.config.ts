60af59c3cbe08b2fedddb21a2a822781067c443f
Penelope Pressman <110858570+PenPressman@users.noreply.github.com>
2026-03-09 16:56:07 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: '/PenPressman/',  // ← add this line
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
@@ -19,3 +21,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
